1ba830d42b0da504e9d63b516fca702f18ad94a6
Oleksii Maslov <wz131h@gmail.com>
2023-01-19 16:38:33 +0200

    js

---
+++ script.js
@@ -0,0 +1,17 @@
+var num_1 = 5.4;
+var num_2 = 15;
+var res = num_1 - num_2;
+console.log("Vibnimannja: " + num_1 * num_2);
+
+var num_3 = 5;
+num_3++;
+
+console.log("Vibnimannja: " + num_3);
+
+var str_1 = Number("12");
+var str_2 = Number("2");
+console.log("Vibnimannja: " + (str_1 + str_2));
+
+console.log("Math: " + Math.PI);
+console.log("Math: " + Math.E);
+console.log("Math: " + Math.max(4, 5, 6, -1, 8));
