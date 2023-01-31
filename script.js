1ffaeee1094f305684d5189f066324690bffe830
Oleksii Maslov <wz131h@gmail.com>
2023-01-31 19:56:05 +0200

    learned js

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-var num_1 = 5.4;
-var num_2 = 15;
-var res = num_1 - num_2;
-console.log("Vibnimannja: " + num_1 * num_2);
+class Person {
+  constructor(name, age, happiness) {
+    this.name = name;
+    this.age = age;
+    this.happiness = happiness;
+  }
 
-var num_3 = 5;
-num_3++;
-
-console.log("Vibnimannja: " + num_3);
-
-var str_1 = Number("12");
-var str_2 = Number("2");
-console.log("Vibnimannja: " + (str_1 + str_2));
-
-console.log("Math: " + Math.PI);
-console.log("Math: " + Math.E);
-console.log("Math: " + Math.max(4, 5, 6, -1, 8));
-
-var number = "15";
-
-if (number != 15) {
-  console.log("Ok");
+  info() {
+    console.log("Людина: " + this.name + ". Рік: " + this.age);
+  }
 }
+
+var alex = new Person("Alex", 45, true);
+var bob = new Person("Bob", 25, false);
+
+alex.name = "alex2";
+alex.info();
+bob.info();
